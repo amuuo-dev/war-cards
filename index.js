e0e9a197d6262f72ec21d128998667580897e0cd
amuuo-dev <anthonmuuo92@gmail.com>
2024-03-10 04:15:01 +0300

    created an anonymous callback function as a separate function, then pass it as the 2nd parameter to our addEventListener

--- index.js
+++ index.js
@@ -1,8 +1,12 @@
-// add function to the button to draw new cards from api
-document.getElementById('new-deck').addEventListener('click',function(){
+// 1. Create a new deck of cards
+
+function newDeck(){
     fetch("https://apis.scrimba.com/deckofcards/api/deck/new/shuffle/")
     .then(res => res.json())
     .then(data => {
        console.log(data)
     })
-})
+}
+//Define our anonymous callback function as a separate function, then pass it as the 2nd parameter to our `addEventListener`
+// add function to the button to draw new cards from api
+document.getElementById('new-deck').addEventListener('click', newDeck)
