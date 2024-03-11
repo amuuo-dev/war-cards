09d9aeef31693ab5bcf8e5d3f73b78dac2bc73e9
amuuo-dev <anthonmuuo92@gmail.com>
2024-03-11 04:24:23 +0300

    grabed the draw button ad when clicked to draw new cards from the deck

--- index.js
+++ index.js
@@ -5,12 +5,22 @@ function newDeck(){
     fetch("https://apis.scrimba.com/deckofcards/api/deck/new/shuffle/")
     .then(res => res.json())
     .then(data => {
-        //store the deck_id in a variable
-        deckId = data.deck_id
-        console.log(deckId)
        console.log(data)
+         //store the deck_id in a variable
+       deckId = data.deck_id
+       console.log(deckId)
     })
 }
 //Define our anonymous callback function as a separate function, then pass it as the 2nd parameter to our `addEventListener`
 // add function to the button to draw new cards from api
 document.getElementById('new-deck').addEventListener('click', newDeck)
+// draw two new cards from the deck
+document.getElementById('draw-cards').addEventListener('click', function(){
+    fetch(`https://apis.scrimba.com/deckofcards/api/deck/${deckId}/draw/?count=2`)
+    .then(res => res.json())
+    .then(data => {
+        console.log(data)
+    })
+})
+   
+
