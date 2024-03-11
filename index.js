1a953d076f81cf4a40c094395d3b6162f961bb22
amuuo-dev <anthonmuuo92@gmail.com>
2024-03-11 05:59:30 +0300

    refored the css so that the cards-slots holds the cards

--- index.js
+++ index.js
@@ -1,5 +1,8 @@
 //setting deck_id to empty string
 let deckId = "";
+const cardsContainer = document.getElementById("cards")
+const newDeckBtn = document.getElementById("new-deck")
+const drawCardBtn = document.getElementById("draw-cards")
 // 1. Create a new deck of cards
 function newDeck(){
     fetch("https://apis.scrimba.com/deckofcards/api/deck/new/shuffle/")
@@ -13,19 +16,20 @@ function newDeck(){
 }
 //Define our anonymous callback function as a separate function, then pass it as the 2nd parameter to our `addEventListener`
 // add function to the button to draw new cards from api
-document.getElementById('new-deck').addEventListener('click', newDeck)
+newDeckBtn.addEventListener('click', newDeck)
 // draw two new cards from the deck
-document.getElementById('draw-cards').addEventListener('click', function(){
+drawCardBtn.addEventListener('click', function(){
     fetch(`https://apis.scrimba.com/deckofcards/api/deck/${deckId}/draw/?count=2`)
     .then(res => res.json())
     .then(data => {
-        console.log(data)
+        console.log(data.cards)
         //display the cards on the page
-        document.getElementById('cards').innerHTML = `
+        cardsContainer.children[0].innerHTML = `
         <img src=${data.cards[0].image} class="card" />
+    `
+    cardsContainer.children[1].innerHTML = `
         <img src=${data.cards[1].image} class="card" />
-        
-        `
+    `
     })
 })
    
